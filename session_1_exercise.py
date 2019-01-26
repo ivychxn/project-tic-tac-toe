8037fc27e198ae9d02fd90aba545358d8f7e8dc0
danthedolphin <dnltrng@gmail.com>
2019-01-26 01:10:37 -0800

    camelcase

--- session_1_exercise.py
+++ session_1_exercise.py
@@ -1,18 +1,18 @@
 import random
 
-def drawBoard(board):
+def draw_board(board):
     # This function prints out the board that it was passed and should not return anything
     #HINT: pass a list to this function that holds the position of 'X's and 'O's
     return
 
-def inputPlayerLetter():
+def input_player_letter():
     #Ask the player if they want to be 'X' or 'O'
     #Return the chosen letter
     #Hint: Use a loop to keep prompting the player for a letter if the input is not valid
 
     return
 
-def whoGoesFirst():
+def who_goes_first():
     #Randomly choose who goes first
     #Return the string 'computer' or 'player'
     #HINT: look up the documentation for the 'random' library
